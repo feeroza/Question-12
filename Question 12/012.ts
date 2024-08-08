df7236e9168f4764629e368f45f30c89c5856497
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 22:05:31 +0500

    Add files via upload

---
+++ Question 12/012.ts
@@ -0,0 +1,11 @@
+// 12. Greetings: Start with the array you used in Exercise 11, but instead of just printing each
+// person’s name, print a message to them. The text of each message
+//should be the same, but each message should be personalized with the person’s name.
+
+
+let names= [ "bisma","fatima","eisha"];
+let message: string = ", can i helpe you?"
+
+ console.log(names [0] + message);
+ console.log(names [2] + message);
+ console.log(names [1] + message);
